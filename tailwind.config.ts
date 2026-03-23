965b4ac28ae4121be212b3fe60c649613c3b77e1
Maggnum <yonatan2804@gmail.com>
2026-03-23 08:53:28 +0200

    themes

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
+
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+
+  theme: {
+    extend: {
+      borderRadius: {
+        sm: "var(--radius-sm)",
+        md: "var(--radius-md)",
+        lg: "var(--radius-lg)",
+        xl: "var(--radius-xl)",
+        "2xl": "var(--radius-2xl)",
+      },
+    },
+  },
+
+  plugins: [],
+};
+
+export default config;
